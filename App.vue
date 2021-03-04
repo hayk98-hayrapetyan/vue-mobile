6dff38b0713d65688091c1ce70117f1ee760e1cf
Hayk Hayrapetyan <65774079+hayk98-hayrapetyan@users.noreply.github.com>
2021-03-05 00:35:39 +0400

    set up project to github

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">My Vue Native App</text>
+    <image
+      :source="require(`./assets/icon.png`)"
+      :style="{width: 200, height: 200}"
+    />
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: blue;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
